dee7bb16bf60da624754ecb4d96f95f1165c6a00
bforrester722 <b.forrester@live.com>
2023-09-18 21:25:58 -0500

    finishes wiki

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('wiki/<str:title>', views.entry, name="entry"),
     path('edit/<str:title>', views.edit, name="edit"),
     path("create", views.create, name="create"),
-    path("random", views.random, name="random")
+    path("random", views.random, name="random"),
+    path("search", views.search, name="search")
 ]
